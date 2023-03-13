f9c1c166c3d28aa78cf2866bd1ffc255aa96d7bc
Luna <luna.software.development@gmail.com>
2023-03-13 10:25:54 +0100

    game

---
+++ pacman/Assets/scripts/pacmansalsa.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pacmansalsa : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey (KeyCode.W)) {  
+            transform.Translate (0f, 0.01f, 0f);      
+        }  
+        if (Input.GetKey (KeyCode.S)) {  
+            transform.Translate (0f, -0.01f, 0f);      
+        }  
+        if (Input.GetKey (KeyCode.D)) {  
+            transform.Translate (0f, 0f, 0.01f);       
+        }  
+        if (Input.GetKey (KeyCode.A)) {  
+            transform.Translate (0f, 0f, -0.01f);     
+        } 
+
+
+        if (Input.GetKey (KeyCode.UpArrow)) {  
+            transform.Translate (0f, 0.01f, 0f);      
+        }  
+        if (Input.GetKey (KeyCode.DownArrow)) {  
+            transform.Translate (0f, -0.01f, 0f);      
+        }
+        if (Input.GetKey (KeyCode.RightArrow)) {  
+            transform.Translate (0, 0f, 0.01f);       
+        }
+        if (Input.GetKey (KeyCode.LeftArrow)) {  
+            transform.Translate (0f, 0f, -0.01f);     
+        }  
+    }
+}
