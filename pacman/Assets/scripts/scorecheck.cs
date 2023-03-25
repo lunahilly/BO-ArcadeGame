e26438ddc4357434a3ed83287380b6d740bc5a37
Luna <luna.software.development@gmail.com>
2023-03-25 13:27:05 +0100

    versie 1

---
+++ pacman/Assets/scripts/scorecheck.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scorecheck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int highscore = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
